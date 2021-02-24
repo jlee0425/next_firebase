0e3996b50ab4694c57a9a322d3fba76fb23fb387
jlee0425 <jake.jklee425@gmail.com>
2021-02-24 21:27:08 +0900

    ISR

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,6 +1,48 @@
 import React from 'react';
+import {
+	firestore,
+	getUserWithUsername,
+	postToJSON,
+} from '../../utils/firebase';
 
 interface Props {}
+
+export const getStaticProps = async ({ params }) => {
+	const { username, slug } = params;
+	const userDoc = await getUserWithUsername(username);
+
+	let post;
+	let path;
+
+	if (userDoc) {
+		const postRef = userDoc.ref.collection('posts').doc(slug);
+		post = postToJSON(await postRef.get());
+
+		path = postRef.path;
+	}
+
+	return {
+		props: { post, path },
+		revalidate: 5000,
+	};
+};
+
+export const getStaticPath = async () => {
+	const snapshot = await firestore.collectionGroup('posts').get();
+
+	const paths = snapshot.docs.map((doc) => {
+		const { slug, username } = doc.data();
+		return {
+			params: { username, slug },
+		};
+	});
+
+	return {
+		paths,
+		fallback: 'blocking', // falls back to regular ssr if the page hasn't been rendered yet
+		// once rendered, the page gets cached.
+	};
+};
 
 const UserPage = (props: Props) => {
 	return (
